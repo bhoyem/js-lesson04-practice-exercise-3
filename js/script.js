6c1c12833d82548f7c1f70b9f52b7bc9c3d0d6cd
Bryan Hoyem <167017808+bhoyem@users.noreply.github.com>
2024-06-13 15:30:47 -0500

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// Change Instagram icon color
+var instagram = document.querySelector(".fa-instagram");
+instagram.style.color = "tomato";
+
+// Add background image
+var foodImage = document.querySelector(".food");
+// console.log(foodImage.outerHTML);
+foodImage.style.backgroundImage = 'url("/img/victoria-shes-unsplash.jpg")';
+
+// Dotted border-bottom around h1
+var noms = document.querySelector("h1");
+noms.style.borderBottom = "4px dotted tomato";
